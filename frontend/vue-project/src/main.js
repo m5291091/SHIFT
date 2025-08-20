be9699d02372bb9da55dce6642e2fdada6c691c1
Kenjo Yamamoto <alphabetagamma@Kenjo.local>
2025-08-20 12:38:32 +0900

    change API

--- frontend/vue-project/src/main.js
+++ frontend/vue-project/src/main.js
@@ -3,9 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axiosInstance from './api/axios'; // Import the configured axios instance
 
 const app = createApp(App)
 
 app.use(router)
+app.config.globalProperties.$axios = axiosInstance; // Make axios available globally via $axios
 
 app.mount('#app')
